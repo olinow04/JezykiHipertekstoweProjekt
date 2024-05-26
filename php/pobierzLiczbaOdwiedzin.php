2ba4f75d2160499b9e118f846f160851e862403c
Oliwier <olinow@outlook.com>
2024-05-26 22:58:43 +0200

    Update pobierzLiczbaOdwiedzin.php

--- php/pobierzLiczbaOdwiedzin.php
+++ php/pobierzLiczbaOdwiedzin.php
@@ -1,24 +1,23 @@
 <?php
 session_start();
 
+//połączenie z bazą danych- najwygodniej wstawić za pomocą require("plik.php");
+require_once "connectionSettings.php";
+
+$conn = mysqli_connect($host, $db_user, $db_password, $db_name);
+
+if (!$conn) {
+    exit('Nie można połączyć z bazą danych' . mysqli_connect_error());
+}
+
 if (!isset($_SESSION['odwiedzony'])) {
     $_SESSION['liczbaOdwiedzin'] = 0;
-
-    //połączenie z bazą danych- najwygodniej wstawić za pomocą require("plik.php");
-    require_once "connectionSettings.php";
-
-    $conn = mysqli_connect($host, $db_user, $db_password, $db_name);
-
-    if (!$conn) {
-        echo ('Nie można połączyć z bazą danych' . mysqli_connect_error());
-        exit;
-    }
 
     //wysłanie zapytania--------------------------------------------
     $sql = "SELECT liczba FROM odwiedziny WHERE id=1"; //zdefiniowanie zabytania w SQL, np. insert, select, describe...
     $result = mysqli_query($conn, $sql); //wysłanie zapytania do bazy
     if (!$result) {
-        echo "Błąd zapytania: " . mysqli_error($conn);
+        exit("Błąd zapytania: " . mysqli_error($conn));
     }
 
     $liczbaOdwiedzin = 0;
@@ -29,21 +28,37 @@ if (!isset($_SESSION['odwiedzony'])) {
             break;
         }
     } else {
-        echo "Nie znaleziono rekordów";
+        exit("Nie znaleziono rekordów");
     }
 
     $liczbaOdwiedzin++;
     $sql = "UPDATE odwiedziny SET liczba = $liczbaOdwiedzin WHERE id=1"; //zdefiniowanie zabytania w SQL, np. insert, select, describe...
     $result = mysqli_query($conn, $sql); //wysłanie zapytania do bazy
     if (!$result) {
-        echo "Błąd zapytania: " . mysqli_error($conn);
+        exit("Błąd zapytania: " . mysqli_error($conn));
     }
-
-    //zamknięcie połączenia---------------------------------------------
-    mysqli_close($conn);
-
 
     $_SESSION['liczbaOdwiedzin'] = $liczbaOdwiedzin;
     $_SESSION['odwiedzony'] = true;
+} else {
+    //wysłanie zapytania--------------------------------------------
+    $sql = "SELECT liczba FROM odwiedziny WHERE id=1"; //zdefiniowanie zabytania w SQL, np. insert, select, describe...
+    $result = mysqli_query($conn, $sql); //wysłanie zapytania do bazy
+    if (!$result) {
+        exit("Błąd zapytania: " . mysqli_error($conn));
+    }
+
+    //wyświetlenie wyników, w zapytaniu select można pobrać wyniki
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $_SESSION['liczbaOdwiedzin'] = $row['liczba'];
+            break;
+        }
+    } else {
+        exit("Nie znaleziono rekordów");
+    }
 }
+
+//zamknięcie połączenia---------------------------------------------
+mysqli_close($conn);
 ?>
