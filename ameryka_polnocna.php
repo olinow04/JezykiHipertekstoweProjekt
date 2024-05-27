cd77bbca8b6fd417389fcdeb8ef8d8806d8b31f2
Oliwier <olinow@outlook.com>
2024-05-27 02:20:13 +0200

    Add subpages

--- ameryka_polnocna.php
+++ ameryka_polnocna.php
@@ -17,7 +17,7 @@
         integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
+    </script>
     <script src="https://kit.fontawesome.com/c1354519af.js" crossorigin="anonymous"></script>
     <script src="js\showToast.js"></script>
     <script src="js\setCurrentSubpage.js"></script>
